9dd153312e10eb797802055beb21cad903d36eef
Parker <pkroberg@gmail.com>
2023-08-13 23:56:38 -0600

    Added modal CSS

--- dist/app.js
+++ dist/app.js
@@ -93,7 +93,7 @@ updateIframe();
 //popup
 function toggleModal(modalID) {
     document.getElementById(modalID).classList.toggle("hidden");
-    document.getElementById(modalID+"-backdrop").classList.toggle("hidden");
+    // document.getElementById(modalID+"-backdrop").classList.toggle("hidden");
     document.getElementById(modalID).classList.toggle("flex");
-    document.getElementById(modalID+"-backdrop").classList.toggle("flex");
+    // document.getElementById(modalID+"-backdrop").classList.toggle("flex");
 }
